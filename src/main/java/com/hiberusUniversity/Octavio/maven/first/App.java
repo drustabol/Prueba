39df05bc6417efe0cca48418318c23fdf2952618
Octavio <drustabol@gmail.com>
2022-04-27 09:25:27 +0200

    Cambio de syso

--- src/main/java/com/hiberusUniversity/Octavio/maven/first/App.java
+++ src/main/java/com/hiberusUniversity/Octavio/maven/first/App.java
@@ -6,6 +6,6 @@ package com.hiberusUniversity.Octavio.maven.first;
  */
 public class App {
     public static void main( String[] args ){
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World! and rock & roll" );
     }
 }
